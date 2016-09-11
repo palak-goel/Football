7e92aedf6e122c4b5f9900232b7720f1a23540df
jramos70 <jerome_ramos@brown.edu>
2016-09-11 17:11:23 -0400

    Add files via upload
    Fixed code to include all qb's.

--- football/nflPlayer.py
+++ football/nflPlayer.py
@@ -5,9 +5,9 @@ import sqlite3
 
 conn = sqlite3.connect('nflPlayers.db')
 c = conn.cursor()
-
+divider = 24 
 def create_qb_table():
-    c.execute('CREATE TABLE IF NOT EXISTS QuarterBacks(name TEXT PRIMARY KEY, team TEXT, games TEXT, qbRating TEXT, completions TEXT, attempts TEXT, passingPercentage TEXT, passingYards TEXT, passYdsPerGame TEXT, passYdsPerAttempt TEXT, passTDs TEXT, interceptions TEXT, rushes TEXT, rushYds TEXT, rushYdsPerGame TEXT, rushYdsPerAttempt TEXT, rushTDs TEXT, sacks TEXT, ydsLost TEXT, fumbles TEXT, fumblesLost TEXT)')
+    c.execute('CREATE TABLE IF NOT EXISTS QuarterBacks(name TEXT PRIMARY KEY, team TEXT, games TEXT, qbRating TEXT, completions TEXT, attempts TEXT, passingYards TEXT, passYdsPerAttempt TEXT, longestPass TEXT, interceptions TEXT, passTDS TEXT, rushes TEXT, rushYds TEXT, rushYdsPerAttempt TEXT, longestRush TEXT, rushTDs TEXT, sacks TEXT, ydsLost TEXT, fumbles TEXT, fumblesLost TEXT)')
 
 def qb_in_table():
 	qb = getQBRatings()
@@ -18,29 +18,27 @@ def qb_in_table():
 		qbRating = stats[3]
 		passCompletions = stats[4]
 		passAttempts =  stats[5]
-		passingPercentage = stats[6]
-		passingYards = stats[7]
-		passYdsPerGame = stats[8]
-		passingYdsPerAttempt = stats[9]
+		passingYards = stats[6]
+		passingYdsPerAttempt = stats[7]
+		passingLongestYds = stats[8]
+		interceptions = stats[9]
 		passingTDs = stats[10]
-		interceptions = stats[11]
-		rushAttempts = stats[13]
-		rushingYds = stats[14]
-		rushYdsPerGame = stats[15]
-		avgRush = stats[16]
-		rushTDs = stats[17]
-		sacks = stats[19]
-		ydsLost = stats[20]
-		fumbles = stats[22]
-		fumblesLost = stats[23]
+		rushAttempts = stats[12]
+		rushingYds = stats[13]
+		avgRush = stats[14]
+		rushingLongestYds = stats[15]
+		rushTDs = stats[16]
+		sacks = stats[18]
+		ydsLost = stats[19]
+		fumbles = stats[21]
+		fumblesLost = stats[22]
 
-		c.execute("INSERT INTO QuarterBacks(name,team,games,qbRating,completions,attempts,passingPercentage,passingYards,passYdsPerGame,passYdsPerAttempt,passTDs,interceptions,rushes,rushYds,rushYdsPerGame,rushYdsPerAttempt,rushTDs,sacks,ydsLost,fumbles,fumblesLost) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
-			(name,team,games,qbRating, passCompletions, passAttempts, passingPercentage, passingYards, passYdsPerGame, passingYdsPerAttempt, passingTDs, interceptions, rushAttempts, rushingYds, rushYdsPerGame, avgRush, rushTDs, sacks, ydsLost, fumbles, fumblesLost))
+		c.execute("INSERT INTO QuarterBacks(name, team, games, qbRating, completions, attempts, passingYards, passYdsPerAttempt, longestPass, interceptions, passTDS, rushes, rushYds, rushYdsPerAttempt, longestRush, rushTDs, sacks, ydsLost, fumbles, fumblesLost) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+			(name, team, games, qbRating, passCompletions, passAttempts, passingYards, passingYdsPerAttempt, passingLongestYds, interceptions, passingTDs, rushAttempts, rushingYds, avgRush, rushingLongestYds, rushTDs, sacks, ydsLost, fumbles, fumblesLost))
 		conn.commit()
-	print(stats)
 
 def getQBRatings():
-	r = requests.get("https://sports.yahoo.com/nfl/stats/byposition?pos=QB&conference=NFL&year=season_2016&timeframe=ToDate&qualified=1&sort=49&old_category=QB")
+	r = requests.get("http://sports.yahoo.com/nfl/stats/byposition?pos=QB")
 	soup = BeautifulSoup(r.content,"html.parser")
 	
 	#QB Ratings
@@ -52,10 +50,9 @@ def getQBRatings():
 
 	#Other Stats 
 	statisticsSearch = soup.find_all('td', {'class':re.compile(r'yspscores')})
-
-	numQBs = len(statisticsSearch)/25
+	numQBs = len(statisticsSearch)/divider
 	old_count = 0
-	new_count = 25
+	new_count = divider
 	qbNum = 0
 
 	player = []
@@ -68,11 +65,10 @@ def getQBRatings():
 				el_stats.append(qbRatings[qbNum])
 			else:
 				el_stats.append((cleanUp2(cleanUp(str(statisticsSearch[i])))))
-		old_count += 25
-		new_count += 25
+		old_count += divider
+		new_count += divider
 		qbNum += 1
 		player.append(el_stats)
-
 	return player
 
 def cleanUp(stat):
